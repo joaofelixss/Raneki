c405f7f2e35d96c1c05f967bf4ba780374b31eba
João Félix <felixjoao651@gmail.com>
2023-07-04 10:12:27 -0400

    first commi

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from '../componentes/Header';
+import Footer from '../componentes/Footer';
+import Produtos from '../componentes/Produtos';
+import Contato from '../Componentes/Contato';
+import Produto from '../Componentes/Produto';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />}></Route>
+            <Route path="produto/:id" element={<Produto />}></Route>
+            <Route path="Contato" element={<Contato />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
